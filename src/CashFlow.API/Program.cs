e142806a2ba446482d15780a517d89c41b4d6582
JonatasGdeC <jonatas.07022003@gmail.com>
2025-03-02 14:59:56 -0300

    Iniciando projeto

---
+++ src/CashFlow.API/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
+builder.Services.AddRouting(option => option.LowercaseUrls = true);
+builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+  app.MapOpenApi();
+  app.UseSwagger();
+  app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
