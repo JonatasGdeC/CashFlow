ae7ade32c1ee9f6e3cdb6e0e8d6fbccd4a9dd9e7
JonatasGdeC <jonatas.07022003@gmail.com>
2025-03-08 14:08:45 -0300

    Aplicando required em 'ErrorMessage'

--- src/CashFlow.Communication/Responses/ResponseErrorJson.cs
+++ src/CashFlow.Communication/Responses/ResponseErrorJson.cs
@@ -2,5 +2,5 @@ namespace CashFlow.Communication.Responses;
 
 public class ResponseErrorJson
 {
-  public string ErrorMessage { get; set; } = string.Empty;
+  public required string ErrorMessage { get; set; }
 }
