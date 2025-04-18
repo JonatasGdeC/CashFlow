e4dac28dc6385983895e76b77e53f24bd3cc3d69
JonatasGdeC <jonatas.07022003@gmail.com>
2025-04-18 15:55:13 -0300

    Aplicando builder em teste unitário

--- tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -1,6 +1,5 @@
 using CashFlow.Application.UseCases.Expenses.Register;
-using CashFlow.Communication.Enums;
-using CashFlow.Communication.Requests;
+using CommonTestUtilities.Requests;
 
 namespace Validators.Tests.Expenses.Register;
 
@@ -11,14 +10,7 @@ public class RegisterExpenseValidatorTests
   {
     //Arrange
     var validator = new RegisterExpenseValidator();
-    var request = new RequestRegisterExpenseJson
-    {
-      Title = "Title",
-      Description = "Description",
-      Date = DateTime.Now.AddDays(-1),
-      Amount = 100,
-      PaymentType = PaymentType.Cash
-    };
+    var request = RequestRegisterExpenseJsonBuilder.Build();
 
     //Act
     var result = validator.Validate(request);
