ece84dfb45575722b7a3997b03077c311e37f49b
JonatasGdeC <jonatas.07022003@gmail.com>
2025-04-19 20:52:17 -0300

    Criando novos testes para erros

--- tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -1,4 +1,5 @@
 using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Enums;
 using CashFlow.Exception;
 using CommonTestUtilities.Requests;
 using Shouldly;
@@ -35,5 +36,55 @@ public class RegisterExpenseValidatorTests
       e => e.Count.ShouldBe(1),
       e => e.First().ErrorMessage.ShouldBe(ResourcesErrorMessages.TITLE_REQUIRED)
       );
+  }
+  
+  [Fact]
+  public void ErrorDateFuture()
+  {
+    var validator = new RegisterExpenseValidator();
+    var request = RequestRegisterExpenseJsonBuilder.Build();
+    request.Date = DateTime.Now.AddDays(1);
+    
+    var result = validator.Validate(request);
+    
+    result.IsValid.ShouldBeFalse();
+    result.Errors.ShouldSatisfyAllConditions(
+      e => e.Count.ShouldBe(1),
+      e => e.First().ErrorMessage.ShouldBe(ResourcesErrorMessages.EXPENSES_CANNOT_FOR_THE_FUTURE)
+    );
+  }
+  
+  [Fact]
+  public void ErrorPaymentTypeInvalid()
+  {
+    var validator = new RegisterExpenseValidator();
+    var request = RequestRegisterExpenseJsonBuilder.Build();
+    request.PaymentType = (PaymentType)700;
+    
+    var result = validator.Validate(request);
+    
+    result.IsValid.ShouldBeFalse();
+    result.Errors.ShouldSatisfyAllConditions(
+      e => e.Count.ShouldBe(1),
+      e => e.First().ErrorMessage.ShouldBe(ResourcesErrorMessages.PAYMENT_TYPE_INVALID)
+    );
+  }
+  
+  [Theory]
+  [InlineData(0)]
+  [InlineData(-1)]
+  public void ErrorAmountInvalid(decimal amount)
+  {
+    var validator = new RegisterExpenseValidator();
+    var request = RequestRegisterExpenseJsonBuilder.Build();
+    request.Amount = amount;
+    
+    var result = validator.Validate(request);
+    
+    result.IsValid.ShouldBeFalse();
+    result.Errors.ShouldSatisfyAllConditions(
+      e => e.Count.ShouldBe(1),
+      e => e.First().ErrorMessage.ShouldBe(ResourcesErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_ZERO)
+    );
   }
 }
