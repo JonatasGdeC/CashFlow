6fbb3b33a2018ad3e5d13887271207e90f89ceb9
JonatasGdeC <jonatas.07022003@gmail.com>
2025-04-27 01:00:49 -0300

    Conectando projeto com db

--- src/CashFlow.API/Program.cs
+++ src/CashFlow.API/Program.cs
@@ -1,5 +1,7 @@
 using CashFlow.API.Filters;
 using CashFlow.API.Middleware;
+using CashFlow.Application;
+using CashFlow.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,9 @@ builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
+
+builder.Services.AddInfrastructure();
+builder.Services.AddApplication();
 
 var app = builder.Build();
 
---
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,19 @@
+using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.DataAccess;
+
+internal class CashFlowDbContext : DbContext
+{
+  public DbSet<Expense> Expenses { get; set; }
+
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+  {
+    var connectionString = "Server=localhost;Database=cashflowdb;Uid=jonatasgdec;Pwd=jonatas2003;";
+
+    var version = new Version(8, 0, 41);
+    var serverVersion = new MySqlServerVersion(version);
+
+    optionsBuilder.UseMySql(connectionString, serverVersion);
+  }
+}
