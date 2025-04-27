0acd28b4befa358129c3c73dfdfae2837c6ce2c3
JonatasGdeC <jonatas.07022003@gmail.com>
2025-04-27 01:20:25 -0300

    Utilizando AppSettings para a Connection string

--- src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -5,15 +5,7 @@ namespace CashFlow.Infrastructure.DataAccess;
 
 internal class CashFlowDbContext : DbContext
 {
+  public CashFlowDbContext(DbContextOptions options) : base(options) { }
+
   public DbSet<Expense> Expenses { get; set; }
-
-  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-  {
-    var connectionString = "Server=localhost;Database=cashflowdb;Uid=jonatasgdec;Pwd=jonatas2003;";
-
-    var version = new Version(8, 0, 41);
-    var serverVersion = new MySqlServerVersion(version);
-
-    optionsBuilder.UseMySql(connectionString, serverVersion);
-  }
 }
