1a3a4a369c9dafa84e3600ce3817ddbeacfb6ad1
JonatasGdeC <jonatas.07022003@gmail.com>
2026-05-14 14:40:08 -0300

    Configurando autenticação

--- src/CashFlow.Api/Program.cs
+++ src/CashFlow.Api/Program.cs
@@ -1,8 +1,11 @@
+using System.Text;
 using CashFlow.Api.Filters;
 using CashFlow.Api.Middleware;
 using CashFlow.Application;
 using CashFlow.Infrastructure;
 using CashFlow.Infrastructure.DataAccess.Migrations;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args: args);
 
@@ -14,7 +17,25 @@ builder.Services.AddMvc(setupAction: options => options.Filters.Add(filterType: 
 builder.Services.AddInfrastructure(configuration: builder.Configuration, connectionString: builder.Configuration.GetConnectionString(name: "connection")!);
 builder.Services.AddApplication();
 
+builder.Services.AddAuthentication(configureOptions: config =>
+{
+    config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(configureOptions: config =>
+{
+    config.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ClockSkew = TimeSpan.Zero,
+        IssuerSigningKey = new SymmetricSecurityKey(key: Encoding.UTF8.GetBytes(s: builder.Configuration.GetValue<string>(key: "Settings:Jwt:SigningKey")!))
+    };
+});
+
 WebApplication app = builder.Build();
+
+app.UseAuthorization();
+app.UseAuthentication();
 
 app.UseMiddleware<CultureMiddleware>();
 
