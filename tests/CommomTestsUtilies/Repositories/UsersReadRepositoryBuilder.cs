8b5f5c4e25a502ce8b8bc584b01771539ee46517
JonatasGdeC <jonatas.07022003@gmail.com>
2026-05-15 01:20:36 -0300

    Criando testes Error_Email_Already_Exists

--- tests/CommomTestsUtilies/Repositories/UsersReadRepositoryBuilder.cs
+++ tests/CommomTestsUtilies/Repositories/UsersReadRepositoryBuilder.cs
@@ -11,6 +11,11 @@ public class UsersReadRepositoryBuilder
     {
         _repositoryMock = new Mock<IUsersReadRepository>();
     }
+
+    public void ExistsUsersWithThisEmail(string email)
+    {
+        _repositoryMock.Setup(expression: repository => repository.ExistsUsersWithThisEmail(email)).ReturnsAsync(value: true);
+    }
     
     public IUsersReadRepository Build() => _repositoryMock.Object;
 }
--- tests/UseCases.Tests/User/Register/RegisterUserUseCaseTest.cs
+++ tests/UseCases.Tests/User/Register/RegisterUserUseCaseTest.cs
@@ -45,21 +45,38 @@ public class RegisterUserUseCaseTest
         ExceptionAssertions<ErrorOnValidationException>? result = await act.Should().ThrowAsync<ErrorOnValidationException>();
         result.Where(exceptionExpression: ex => ex.GetErrors().Count == 1 && ex.GetErrors().Contains(ResourceErrorMessage.USER_NAME_REQUIRED));
     }
+    
+    [Fact]
+    public async Task Error_Email_Already_Exists()
+    {
+        RequestRegisterUserJson request = RequestRegisterUserJsonBuilder.Build();
+        RegisterUserUseCase useCase = CreateUseCase(email: request.Email);
+        
+        Func<Task<ResponseRegisterUserJson>> act = async () => await useCase.Execute(request: request);
 
-    private RegisterUserUseCase CreateUseCase()
+        ExceptionAssertions<ErrorOnValidationException>? result = await act.Should().ThrowAsync<ErrorOnValidationException>();
+        result.Where(exceptionExpression: ex => ex.GetErrors().Count == 1 && ex.GetErrors().Contains(ResourceErrorMessage.USER_EMAIL_ALREADY_EXIST));
+    }
+
+    private RegisterUserUseCase CreateUseCase(string? email = null)
     {
         IMapper mapper = MapperBuilder.Build();
         IUnitOfWork unitOfWork = UnitOfWorkBuilder.Build();
         IUsersWriteRepository writeRepository = UsersWriteRepositoryBuilder.Build();
         IAccessTokenGenerator accessTokenGenerator = JwtTokenGeneratorBuilder.Build();
         IPasswordEncrypter passwordEncrypter = PasswordEncrypterBuilder.Build();
-        IUsersReadRepository readRepository = new UsersReadRepositoryBuilder().Build();
+        UsersReadRepositoryBuilder readRepository = new();
+
+        if (!string.IsNullOrEmpty(value: email))
+        {
+            readRepository.ExistsUsersWithThisEmail(email: email);
+        }
 
         return new RegisterUserUseCase(
             mapper: mapper, 
             unitOfWork: unitOfWork, 
             writeRepository: writeRepository, 
-            readRepository: readRepository,
+            readRepository: readRepository.Build(),
             tokenGenerator: accessTokenGenerator, 
             encrypter: passwordEncrypter);
     }
