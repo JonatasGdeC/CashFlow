14b24149d53ab4eadad4de614c0a098b6b4d599c
JonatasGdeC <jonatas.07022003@gmail.com>
2026-05-16 00:37:16 -0300

    Criando teste de integração com sucesso

--- tests/WebApi.Tests/Users/Register/RegisterUserTest.cs
+++ tests/WebApi.Tests/Users/Register/RegisterUserTest.cs
@@ -1,12 +1,22 @@
+using System.Net;
+using System.Net.Http.Json;
+using CashFlow.Communication.Requests;
+using CommomTestsUtilies.Requests;
+using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace WebApi.Tests.Users.Register;
 
-public class RegisterUserTest : IClassFixture<WebApplicationFactory<Program>>
+public class RegisterUserTest(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>
 {
+    private readonly HttpClient _httpClient = factory.CreateClient();
+
     [Fact]
     public async Task Success()
     {
-        
+        RequestRegisterUserJson request = RequestRegisterUserJsonBuilder.Build();
+        HttpResponseMessage result = await _httpClient.PostAsJsonAsync(requestUri: "api/User", value: request);
+
+        result.StatusCode.Should().Be(expected: HttpStatusCode.Created);
     }
 }
