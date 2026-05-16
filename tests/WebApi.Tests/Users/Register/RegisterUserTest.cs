2408b7b05777b91afe1496796be2e38ab9090049
JonatasGdeC <jonatas.07022003@gmail.com>
2026-05-16 20:26:06 -0300

    Concluindo testes de integração para o sucesso

--- tests/WebApi.Tests/Users/Register/RegisterUserTest.cs
+++ tests/WebApi.Tests/Users/Register/RegisterUserTest.cs
@@ -1,6 +1,8 @@
 using System.Net;
 using System.Net.Http.Json;
+using System.Text.Json;
 using CashFlow.Communication.Requests;
+using CashFlow.Communication.Response;
 using CommomTestsUtilies.Requests;
 using FluentAssertions;
 
@@ -14,8 +16,13 @@ public class RegisterUserTest(CustomWebApplicationFactory factory) : IClassFixtu
     public async Task Success()
     {
         RequestRegisterUserJson request = RequestRegisterUserJsonBuilder.Build();
+        
         HttpResponseMessage result = await _httpClient.PostAsJsonAsync(requestUri: "api/User", value: request);
 
         result.StatusCode.Should().Be(expected: HttpStatusCode.Created);
+        Stream body = await result.Content.ReadAsStreamAsync();
+        JsonDocument response = await JsonDocument.ParseAsync(utf8Json: body);
+        response.RootElement.GetProperty(propertyName: nameof(ResponseRegisterUserJson.Name).ToLower()).GetString().Should().Be(expected: request.Name);
+        response.RootElement.GetProperty(propertyName: nameof(ResponseRegisterUserJson.Token).ToLower()).GetString().Should().NotBeNullOrEmpty();
     }
 }
