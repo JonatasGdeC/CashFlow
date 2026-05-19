e8fd9733d3d5023e5b54be9e19cfb2be871c3404
JonatasGdeC <jonatas.07022003@gmail.com>
2026-05-19 19:55:33 -0300

    Alterando Criar/Atualizar despesa

--- src/CashFlow.Application/UsesCases/Expense/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UsesCases/Expense/Register/RegisterExpenseUseCase.cs
@@ -5,16 +5,19 @@ using CashFlow.Domain.Repositories.Expenses;
 using CashFlow.Exception.ExceptionBase;
 using FluentValidation.Results;
 using CashFlow.Domain.Repositories;
+using CashFlow.Domain.Services.LoggedUser;
 
 namespace CashFlow.Application.UsesCases.Expense.Register;
 
-public class RegisterExpenseUseCase(IExpensesWriteRepository writeRepository, IUnitOfWork unitOfWork, IMapper mapper) : IRegisterExpenseUseCase
+public class RegisterExpenseUseCase(IExpensesWriteRepository writeRepository, IUnitOfWork unitOfWork, IMapper mapper, ILoggedUser loggedUser) : IRegisterExpenseUseCase
 {
     public async Task <ResponseRegisterExpenseJson> Execute(RequestRegisterExpenseJson request)
     {
         Validate(request: request);
-        
+
+        Domain.Enitites.User currentUser = await loggedUser.Get();
         Domain.Enitites.Expense? expense = mapper.Map<Domain.Enitites.Expense>(source: request);
+        expense.UserId = currentUser.Id;
 
         await writeRepository.Add(expense: expense);
         await unitOfWork.Commit();
