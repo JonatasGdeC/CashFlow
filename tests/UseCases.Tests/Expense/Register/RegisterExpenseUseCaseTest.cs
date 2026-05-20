f4a5cf6f2c770f1bebf7627bc09fe4bbcfc21e01
JonatasGdeC <jonatas.07022003@gmail.com>
2026-05-20 00:41:29 -0300

    Criando erro para validação de registro do expense

--- tests/UseCases.Tests/Expense/Register/RegisterExpenseUseCaseTest.cs
+++ tests/UseCases.Tests/Expense/Register/RegisterExpenseUseCaseTest.cs
@@ -4,14 +4,16 @@ using CashFlow.Communication.Requests;
 using CashFlow.Communication.Response;
 using CashFlow.Domain.Repositories;
 using CashFlow.Domain.Repositories.Expenses;
-using CashFlow.Domain.Repositories.Users;
 using CashFlow.Domain.Services.LoggedUser;
+using CashFlow.Exception;
+using CashFlow.Exception.ExceptionBase;
 using CommomTestsUtilies.Entities;
 using CommomTestsUtilies.LoggedUser;
 using CommomTestsUtilies.Mapper;
 using CommomTestsUtilies.Repositories;
 using CommomTestsUtilies.Requests;
 using FluentAssertions;
+using FluentAssertions.Specialized;
 
 namespace UseCases.Tests.Expense.Register;
 
@@ -29,6 +31,20 @@ public class RegisterExpenseUseCaseTest
         result.Should().NotBeNull();
         result.Title.Should().NotBeNullOrWhiteSpace();
         result.Title.Should().Be(expected: request.Title);
+    }
+    
+    [Fact]
+    public async Task Error_Title_Empty()
+    {
+        CashFlow.Domain.Enitites.User loggedUser = UserBuilder.Build();
+        RequestRegisterExpenseJson request = RequestRegisterExpenseJsonBuilder.Build();
+        request.Title = String.Empty;
+        RegisterExpenseUseCase useCase = CreateUseCase(user: loggedUser);
+        
+        Func<Task<ResponseRegisterExpenseJson>> act = async () => await useCase.Execute(request: request);
+
+        ExceptionAssertions<ErrorOnValidationException>? result = await act.Should().ThrowAsync<ErrorOnValidationException>();
+        result.Where(exceptionExpression: ex => ex.GetErrors().Count == 1 && ex.GetErrors().Contains(ResourceErrorMessage.EXPENSE_TITLE_REQUIRED));
     }
     
     private RegisterExpenseUseCase CreateUseCase(CashFlow.Domain.Enitites.User user)
