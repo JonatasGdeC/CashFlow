67981094dca8a039423c3aa42ae8b4a6f2ff7557
JonatasGdeC <jonatas.07022003@gmail.com>
2026-05-23 15:57:36 -0300

    Bugfix em Validate do UpdateUserUseCase

--- src/CashFlow.Application/UsesCases/User/Update/UpdateUserUseCase.cs
+++ src/CashFlow.Application/UsesCases/User/Update/UpdateUserUseCase.cs
@@ -4,28 +4,42 @@ using CashFlow.Communication.Requests;
 using CashFlow.Domain.Repositories;
 using CashFlow.Domain.Repositories.Users;
 using CashFlow.Domain.Services.LoggedUser;
+using CashFlow.Exception;
 using CashFlow.Exception.ExceptionBase;
 using FluentValidation.Results;
 
 namespace CashFlow.Application.UsesCases.User.Update;
 
-public class UpdateUserUseCase(ILoggedUser loggedUser, IMapper mapper, IUsersWriteRepository writeRepository, IUnitOfWork unitOfWork) : IUpdateUserUseCase
+public class UpdateUserUseCase(
+    ILoggedUser loggedUser,
+    IMapper mapper,
+    IUsersWriteRepository writeRepository,
+    IUsersReadRepository readRepository,
+    IUnitOfWork unitOfWork) : IUpdateUserUseCase
 {
     public async Task Execute(RequestUpdateUserJson request)
     {
-        Validate(request: request);
         Domain.Enitites.User user = await loggedUser.Get();
+        await Validate(request: request, currentEmail: user.Email);
         
         mapper.Map(source: request, destination: user);
-        
+
         writeRepository.Update(user: user);
         await unitOfWork.Commit();
     }
-    
-    private void Validate(RequestUpdateUserJson request)
+
+    private async Task Validate(RequestUpdateUserJson request, string currentEmail)
     {
         ValidationResult resultRequest = new RegisterUserValidator().Validate(instance: request);
-
+        
+        if (!currentEmail.Equals(value: request.Email))
+        {
+            Domain.Enitites.User? userExist = await readRepository.GetUserByEmail(email: request.Email);
+            if (userExist != null)
+            {
+                resultRequest.Errors.Add(item: new ValidationFailure(propertyName: string.Empty, errorMessage: ResourceErrorMessage.USER_EMAIL_ALREADY_EXIST));
+            }
+        }
 
         if (!resultRequest.IsValid)
         {
